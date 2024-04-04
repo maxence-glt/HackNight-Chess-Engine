997715e63d14a32aa4d4e56b630da4826a57ce6e
maxence-glt <maxencegilloteaux2023@gmail.com>
2024-04-04 04:20:36 -0400

    basic SDL setup

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include "../include/SDLWindow.h"
+
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+int main() {
+    // simple window function for SDL
+    windowMain();
+}
