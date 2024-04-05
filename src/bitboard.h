9d94d893e1d84a5593646a677646998df62f1bde
maxence-glt <maxencegilloteaux2023@gmail.com>
2024-04-05 16:15:46 -0400

    bitboard began

---
+++ src/bitboard.h
@@ -0,0 +1,16 @@
+/*
+ * https://pages.cs.wisc.edu/~psilord/blog/data/chess-pages/rep.html
+ * A row of a chess board is called a "rank" and a column is called a "file"
+ *
+ * The white side will ALWAYS be on the bottom and the black side will ALWAYS be on top
+ *
+ */
+
+#ifndef HACKNIGHT_CHESS_ENGINE_BITBOARD_H
+#define HACKNIGHT_CHESS_ENGINE_BITBOARD_H
+
+#include "types.h"
+
+Bitboard holder;
+
+#endif
---
+++ src/types.h
@@ -0,0 +1,8 @@
+#ifndef HACKNIGHT_CHESS_ENGINE_TYPES_H
+#define HACKNIGHT_CHESS_ENGINE_TYPES_H
+
+#include <cstdint>
+
+using Bitboard = uint64_t;
+
+#endif //HACKNIGHT_CHESS_ENGINE_TYPES_H
